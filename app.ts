7c27406ddbf92bfd2a15374c620900d73378db27
SYEDSUBHAN11 <syedsubhansarwar03856@gmail.com>
2024-07-18 13:52:36 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,19 @@
+//inquirer
+// import inquirer from "inquirer";
+// let answer = await inquirer.prompt({
+//     name: "name",
+//     type: "input",
+//     message: "enter you name",
+// });
+// console.log(`hello! ${answer.name}`);
+
+//inquirer with chalk
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt({
+        name: "name",
+        type: "input",
+        message:chalk.yellow.italic("enter you name"),
+    });
+    console.log(`hello! ${answer.name}`);
+    
